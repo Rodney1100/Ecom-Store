d0da60a4796dce366997509c7cf5ba412723ec83
Rodney Plancher <90078262+Rodney1100@users.noreply.github.com>
2024-07-24 15:40:29 -0400

    finished user rout mvp

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const User = require("../models/User");
 const CryptoJS = require("crypto-js");
+const jwt = require("jsonwebtoken");
 
 // Register
 
@@ -17,6 +18,37 @@ router.post("/register", async (req, res) => {
   try {
     const savedUser = await newUser.save();
     res.status(201).json(savedUser);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// LOGIN
+
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ username: req.body.username });
+    !user && res.status(401).json("No Username");
+    const hashedPassword = CryptoJS.AES.decrypt(
+      user.password,
+      process.env.PASS_SEC
+    );
+
+    const OGPassword = hashedPassword.toString(CryptoJS.enc.Utf8);
+
+    OGPassword !== req.body.password &&
+      res.status(401).json("wrong username or password");
+    const accessToken = jwt.sign(
+      {
+        id: user._id,
+        isAdmin: user.isAdmin,
+      },
+      process.env.JWT_SEC,
+      { expiresIn: "1d" }
+    );
+    const { password, ...others } = user._doc;
+
+    res.status(200).json({...others, accessToken});
   } catch (err) {
     res.status(500).json(err);
   }
